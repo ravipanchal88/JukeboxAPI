970c7bb45e9b3852b2fa57d3dde9e426ebd51bba
Ravi Panchal <pancha.ravi@gmail.com>
2017-04-02 11:05:40 -0400

    Fixed the song queue functionaililty and other bug fixes

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
   var queuepointer= 0; // Pointer to track the songs in a queue; 
   var playpointer=0;   // Pointer to track the current song in queue; 
 
-
   //Constructor for Jukebox
   function Jukebox(title,artist,albumcover,src,type,funfact) {
     this.title = title;
@@ -13,12 +12,11 @@ $(document).ready(function(){
     this.src = src;
     this.type = type;
     this.funfact = funfact;
-    }
+  }
 
   //Audio play functionailty
   function playAudio() { 
     var currentsongsrc = songsqueue[playpointer].src;
-    var currentsongtype = songsqueue[playpointer].type;
     $("#artist").html(songsqueue[playpointer].artist);
     $("#songtitle").html(songsqueue[playpointer].title);
     $("#genre").html("Rock");
@@ -30,83 +28,64 @@ $(document).ready(function(){
 
   var aud = document.getElementById("myAudio");
   aud.onended = function() {
-        playpointer++;
-        console.log(playpointer);
-        console.log(queuepointer);
-        alert(" I am playing next song");
-        if(playpointer <  queuepointer) {
-        playAudio();
-        buildImages();
-        }
+    playpointer++;
+    if(playpointer <  queuepointer) {
+      playAudio();
+      renderQueue();
+    }
   };
     
-
- // Songs Kist//
-  $("#beatles").click( function(){
-      var beatles = new Jukebox("Beatles","John lennon","beatles1.jpg","keromama.mp3","mpeg","Beatles were the greatest Rock band the world ever saw. The totalsales of beatles records to date is 10 billions dollars");
-      songsqueue[queuepointer] = Object.assign({},beatles);
-      queuepointer++;
-      var x = "images/"+(songsqueue[playpointer].albumcover);
-      $(".queueclass").attr('src', x);
-      buildImages();
+//Below on Click event creates an instance of jukebox object by reading data from HTML 
+  $(".songs").click( function(){
+    var x = this;
+    var song = new Jukebox(x.dataset.title,x.dataset.artist,x.dataset.albumcover,x.dataset.src,x.dataset.type,x.dataset.funfact);
+    songsqueue[queuepointer] = Object.assign({},song);
+    queuepointer++;
+    renderQueue()
   });
-
-  $("#ladygaga").click( function(){
-      var ladygaga = new Jukebox("Alejandro","ladygaga","ladygaga.jpg","song1.mp3","mpeg","Her main nicknames are Gagaloo, Loopy, Mother Monster, Rabbit Teeth and Little Mermaid");
-      songsqueue[queuepointer] = Object.assign({},ladygaga);
-      queuepointer++;
-      buildImages();
-  });
-
-    $("#jayz").click( function(){
-       var jayz = new Jukebox("Alejandro","ladygaga","jayz.jpg","song3.mp3","mpeg","Jay-Z sold crack and used a rubber band as a money clip: The little dudes who were working with me had to earn their rubber bands… make a certain quota for the week to get the rubber band. If they did something that wasn’t thorough, like lost work or put someone on the team at risk, they got their rubber band popped.");
-       songsqueue[queuepointer] = Object.assign({},jayz);
-       queuepointer++;
-      buildImages();
-  });
-
-
 
 //Audio Controls
   $("#playbutton").click(function(){
-       playAudio();
-    });
+    playAudio();
+  });
 
-
-   $("#pausebutton").click(function(){
-      // playAudio();
-        $('#myAudio')[0].pause();
-    });
-
+  $("#pausebutton").click(function(){
+    $('#myAudio')[0].pause();
+  });
 
   $("#stopbutton").click(function(){
-      // playAudio();
-        $('#myAudio')[0].stop();
-    });
-
+    $('#myAudio')[0].stop();
+    $('#myAudio')[0].currentTime = 0;
+  });
 
   $("#nextbutton").click(function(){
-      // playAudio();
-       playpointer++;
-       playAudio();
-    });
+    playpointer++;
+    if(playpointer > (queuepointer-1)){
+    alert("You have reached last song in queue");
+    } else {
+    playAudio();
+    }
+  });
 
 
   $("#prevbutton").click(function(){
-       playpointer--;
-       playAudio();
-    });
+    playpointer--;
+    if(playpointer < 0){
+      alert("You have reached 1st song in queue");
+    } else { 
+    playAudio();
+    }
+  });
 
 //song queue display
-function buildImages() {
-   var j = 0;
-      $('#jukeboxqueue').append ( 
-      j = document.createElement('img'),
-      j.id = "newimg"+[j],
-      j.class = "imgqueue",
-      j.src =  "images/"+(songsqueue[queuepointer-1].albumcover),
-      j.style =  "width:75px;height:75px;"
-    )};
+  function renderQueue() {
+    var j = 0;
+    j = document.createElement('img'),
+    j.src =  "images/"+(songsqueue[queuepointer-1].albumcover)
+    j.style =  "width:75px;height:75px;"
+    $('#jukeboxqueue').append(j);
+  };
+    
 });
 
 
